bc7f53648ed7269a9e20fe0cf4079df7db65be2b
sznzn <sznzn@github.com>
2023-08-02 20:05:44 +0200

    laptop

---
+++ main.js
@@ -0,0 +1,7 @@
+//每次输出一个法语单词
+let words = ["Bonjour", "Au revoir", "Merci", "S'il vous plaît", "Oui", "Non", "Excusez-moi"];
+
+let i = Math.floor(Math.random() * words.length);
+//console.log(i);
+let word = words[i];
+console.log(word);
---
+++ readme.md
@@ -0,0 +1,1 @@
+*un projet sur Git et JS
