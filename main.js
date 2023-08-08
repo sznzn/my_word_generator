4f3d898cd2d1f60d07eca3da7c91fa1f99097d92
sznzn <sznzn@github.com>
2023-08-08 15:22:27 +0200

    laptop

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 //每次输出一个法语单词
 let words = ["Bonjour", "Au revoir", "Merci", "S'il vous plaît", "Oui", "Non", "Excusez-moi"];
 
-let i = Math.floor(Math.random() * words.length);
+// let i = Math.floor(Math.random() * words.length);
 //console.log(i);
-let word = words[i];
-console.log(word);
+// let word = words[i];
+// console.log(word);
+let button = document.getElementById("button");
+button.addEventListener("click", function() {
+    let i = Math.floor(Math.random() * words.length);
+    let word = words[i];
+    console.log(word);
+    document.getElementById("word").innerHTML = word;
+});
